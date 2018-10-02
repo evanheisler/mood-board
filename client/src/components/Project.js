b6c844ccfc81e50d70896c07c9048e68d82cd671
Evan Heisler <evanheisler@gmail.com>
2018-10-02 16:43:03 -0500

    add updated at

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import { ReactComponent as Delete } from 'SVG/recycle.svg';
 
 const Project = ({ data, ...rest }) => {
   return (
     <div className="row">
-      <div className="col pl-4">
+      <div className="col pl-4 py-2">
         <div className="row align-items-center">
           <div className="col-5 mr-auto">
             <h4 className="my-0">{data.name}</h4>
+            <small className="d-block text-muted">
+              Last updated{' '}
+              {moment(data.updatedAt).format('ddd MMM Do, YYYY [at] h:mma')}
+            </small>
           </div>
           {data.status.map(entry => (
             <div className="col-2 text-center" key={`${entry.name}-${data.id}`}>
