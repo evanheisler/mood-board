50cfd962d5da625a1de249864506dff86c32b8ff
Evan Heisler <evanheisler@gmail.com>
2018-10-03 16:55:00 -0500

    fix: try with checked in config and env vars

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+require('fs');
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER_DEV,
-    password: process.env.DB_PASSWORD_DEV,
-    database: process.env.DB_DATABASE_DEV,
-    host: process.env.DB_HOST_DEV,
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: 'postgres',
     ssl: true,
     dialectOptions: {
@@ -13,10 +14,10 @@ module.exports = {
     }
   },
   test: {
-    username: process.env.DB_USER_TEST,
-    password: process.env.DB_PASSWORD_TEST,
-    database: process.env.DB_DATABASE_TEST,
-    host: process.env.DB_HOST_TEST,
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: 'postgres',
     ssl: true,
     dialectOptions: {
